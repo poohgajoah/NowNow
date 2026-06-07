bf4eb5c9056e5d001a8a2ac81473bf0f16c15e84
poohgajoah <seah0rse@gmail.com>
2026-06-08 08:56:02 +0900

    android 이슈

---
+++ src/services/supabase.ts
@@ -0,0 +1,6 @@
+import { createClient } from "@supabase/supabase-js";
+
+const SUPABASE_URL = "https://lvtlyypmxszgjdfzqjta.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx2dGx5eXBteHN6Z2pkZnpxanRhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA4MDU3MzksImV4cCI6MjA5NjM4MTczOX0.uzG1qOquYKvEzoYuOnSjbCOxQqK7xkgRvVz_dNDS2C8";
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
